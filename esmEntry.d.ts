f93345d3fb3883fdb237631a5b4255dc9c2a04ee
sevenc-nanashi <sevenc7c@sevenc7c.com>
2024-08-09 21:03:39 +0900

    Change: 型のエクスポート方法を変える

--- esmEntry.d.ts
+++ esmEntry.d.ts
@@ -1,10 +1,5 @@
 import type { p5 } from "p5";
 
-declare module "p5.capture" {
-  export class P5Capture {
-    initialize(p5: p5): void;
-    postDraw(): void;
-  }
+export function registerP5Capture(p5: p5): void;
 
-  export function registerP5Capture(p5: p5): void;
-}
+export * from "./typesDist/src/p5.capture.d.ts";
